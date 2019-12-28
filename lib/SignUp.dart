1e112bdedbeea6482571e1850959d031a5b992f5
Rahul Khatri <khatri.rahul019@gmail.com>
2019-12-28 18:19:28 +0530

    Wip - Signup, Text Filed Updated

--- lib/SignUp.dart
+++ lib/SignUp.dart
@@ -9,7 +9,17 @@ class SignUp extends StatefulWidget {
   _signUp createState() => new _signUp();
 }
 
-class _signUp extends State<SignUp>{
+class _signUp extends State<SignUp> {
+  int radioValue = 0;
+  double sliderValue = 0;
+
+  void radioMethod(value) {
+    setState(() {
+      radioValue = value;
+      print(radioValue);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
@@ -19,10 +29,148 @@ class _signUp extends State<SignUp>{
       ),
       body: new Column(
         children: <Widget>[
-
+          Align(
+            alignment: Alignment.center,
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: new Image.asset('assets/google_play_100.png'),
+            ),
+          ),
+          Row(
+            children: <Widget>[
+              Padding(
+                  padding: EdgeInsets.all(12.0),
+                  child: Text(
+                    "Name : ",
+                    style: TextStyle(color: Colors.black, fontSize: 16.0),
+                  )),
+              Container(
+                width: 200,
+                child: TextField(
+                  decoration: new InputDecoration(
+                      hintText: "Enter Full Name", labelText: "Full Name"),
+                ),
+              ),
+            ],
+          ),
+          Row(
+            children: <Widget>[
+              Padding(
+                  padding: EdgeInsets.all(12.0),
+                  child: Text(
+                    "Profession",
+                    style: TextStyle(color: Colors.black, fontSize: 16.0),
+                  )),
+              Column(
+                children: <Widget>[
+                  Row(
+                    children: <Widget>[
+                      Radio(
+                          value: 1,
+                          groupValue: radioValue,
+                          onChanged: (int rv) {
+                            radioMethod(rv);
+                          }),
+                      Text(
+                        "Professional",
+                        style: TextStyle(color: Colors.black, fontSize: 16.0),
+                      )
+                    ],
+                  ),
+                  Row(
+                    children: <Widget>[
+                      Radio(
+                          value: 2,
+                          groupValue: radioValue,
+                          onChanged: (int rv) {
+                            radioMethod(rv);
+                          }),
+                      Text(
+                        "Student",
+                        style: TextStyle(color: Colors.black, fontSize: 16.0),
+                      )
+                    ],
+                  )
+                ],
+              )
+            ],
+          ),
+          Row(
+            children: <Widget>[
+              Padding(
+                  padding: EdgeInsets.all(12.0),
+                  child: Text(
+                    "Intrest in \nLanguage :",
+                    style: TextStyle(color: Colors.black, fontSize: 16.0),
+                  )),
+              Container(
+                width: 240,
+                child: Wrap(
+                  spacing: 5.0,
+                  runSpacing: 3.0,
+                  children: <Widget>[
+                    filterChipWidget(chipName: 'Python'),
+                    filterChipWidget(chipName: 'Java'),
+                    filterChipWidget(chipName: 'Kotlin'),
+                    filterChipWidget(chipName: 'Dart'),
+                  ],
+                ),
+              )
+            ],
+          ),
+          Row(
+            children: <Widget>[
+              Padding(
+                  padding: EdgeInsets.all(12.0),
+                  child: Text(
+                    "Rate your Self \nin Dart :",
+                    style: TextStyle(color: Colors.black, fontSize: 16.0),
+                  )),
+              new Slider(value: sliderValue, min: 0, max: 5, divisions: 5, onChanged: (double dbVal){
+                setState(() {
+                  sliderValue = dbVal;
+                });
+              }),
+              Text(sliderValue.toString())
+            ],
+          )
         ],
       ),
     );
   }
+}
 
+class filterChipWidget extends StatefulWidget {
+  final String chipName;
+
+  filterChipWidget({Key key, this.chipName}) : super(key: key);
+
+  @override
+  _filterChipWidgetState createState() => _filterChipWidgetState();
+}
+
+class _filterChipWidgetState extends State<filterChipWidget> {
+  var _isSelected = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return FilterChip(
+      label: Text(widget.chipName),
+      labelStyle: TextStyle(
+          color: Color(0xff6200ee),
+          fontSize: 16.0,
+          fontWeight: FontWeight.bold),
+      selected: _isSelected,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(30.0),
+      ),
+      backgroundColor: Color(0xffededed),
+      onSelected: (isSelected) {
+        setState(() {
+          _isSelected = isSelected;
+        });
+      },
+      selectedColor: Color(0xffeadffd),
+    );
+  }
 }
--- lib/input_selection/TextField.dart
+++ lib/input_selection/TextField.dart
@@ -19,19 +19,34 @@ class _textApp extends State<TextFieldApp> {
           backgroundColor: Colors.lime, title: new Text("Text Field App")),
       body: new Column(
         children: <Widget>[
-          new TextField(
-            /* onChanged: (String text){  //  on Changed ie while entering value real time.
+          Align(
+              alignment: Alignment.center,
+              child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text(
+                    "User name : ",
+                    style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 16.0,
+                        fontWeight: FontWeight.bold),
+                  ))),
+          Padding(
+              padding: EdgeInsets.all(8.0),
+              child: new TextField(
+                /* onChanged: (String text){  //  on Changed ie while entering value real time.
               setState(() {
                 _text = text;
               });
             },*/
-            onSubmitted: (String text) {
-              // on submitted ie when submit
-              setState(() {
-                _text = text;
-              });
-            },
-          decoration: new InputDecoration(hintText: "Enter some text", labelText: "Text Label"),),
+                onSubmitted: (String text) {
+                  // on submitted ie when submit
+                  setState(() {
+                    _text = text;
+                  });
+                },
+                decoration: new InputDecoration(
+                    hintText: "Enter User Name", labelText: "User Name"),
+              )),
           new Text(_text)
         ],
       ),
